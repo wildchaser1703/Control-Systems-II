dabe1f429f95d94c3de16b2e49d74e803b1ac724
Toshali Mohapatra <toshalimohapatra1@gmail.com>
2021-04-03 21:44:43 +0530

    experiment 4

---
+++ exp4_1.m
@@ -0,0 +1,13 @@
+
+% 	To Obtain transfer function and its step- response of a State model given by
+% [(x1@x2)]=[(0     1@-1-1)]  [(x1@x2)] + [(0@1)] u
+% 	Y= [ 1	0] [(x1@x2)]
+
+a= [ 0  1; -1  -1]
+b= [0; 1]
+c=[1  0]
+d= 0
+[num, den]= ss2tf(a,b,c,d)
+g=tf (num, den)
+step (g)
+
